5cbb1cfe328184c112b2175eeaa2aec695447298
mukhammmad-irmatov <mukhammad.irmatov@gmail.com>
2023-01-16 13:19:19 +0500

    chnages

--- news_project/settings.py
+++ news_project/settings.py
@@ -9,8 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-3*gia%2uan7-k!^llviaj=)8u=f+sb+1ukawgmxd1j+4@nu4!9"
-
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
